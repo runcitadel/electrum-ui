f064063e7a097307184c2f04295bf469b5735488
AaronDewes <aaron.dewes@protonmail.com>
2022-10-16 19:54:53 +0000

    Remove demo page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,16 +3,14 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/api/syncPercentage.ts";
-import * as $2 from "./routes/index.tsx";
+import * as $0 from "./routes/api/syncPercentage.ts";
+import * as $1 from "./routes/index.tsx";
 import * as $$0 from "./islands/CopyInput.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
-    "./routes/api/syncPercentage.ts": $1,
-    "./routes/index.tsx": $2,
+    "./routes/api/syncPercentage.ts": $0,
+    "./routes/index.tsx": $1,
   },
   islands: {
     "./islands/CopyInput.tsx": $$0,
